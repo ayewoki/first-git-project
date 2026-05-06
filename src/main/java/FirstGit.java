7a67a9eaf6b450f4373efa661e5e517e0a7468ec
ayewoki <rabotautub710@gmail.com>
2026-05-06 13:47:10 +0300

    add class FirstGit and sout and TbI pidr

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,6 +2,13 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("[<GIT HUIT>]");
         System.out.println("[<GIT HUIT LEARNING>]");
+        System.out.println("[<GIT HUIT LEARNING>]");
+        System.out.println("[<GIT HUIT LEARNING>]");
+        System.out.println("[<GIT HUIT LEARNING>]");
+
+
+        int result = 5 + 10;
+        System.out.println(result);
     }
 
 }
