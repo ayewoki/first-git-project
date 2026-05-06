74062a76aa0197d3e615b5ad4329f1242d4af8d5
ayewoki <rabotautub710@gmail.com>
2026-05-06 15:07:42 +0300

    add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,13 +2,9 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("[<GIT HUIT>]");
         System.out.println("[<GIT HUIT LEARNING>]");
-        System.out.println("[<GIT HUIT LEARNING>]");
-        System.out.println("[<GIT HUIT LEARNING>]");
-        System.out.println("[<GIT HUIT LEARNING!>]");
-
+        System.out.println("[<I'm terribly upset>]");
 
         int result = 5 + 10;
         System.out.println(result);
     }
-
 }
