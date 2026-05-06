51c4e83f30899634d7c9917128ff331ef9346eb1
ayewoki <rabotautub710@gmail.com>
2026-05-06 15:05:54 +0300

    Update FirstGit.java
    add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,9 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("[<GIT HUIT>]");
         System.out.println("[<GIT HUIT LEARNING>]");
-        System.out.println("[<GIT HUIT LEARNING>]");
-        System.out.println("[<GIT HUIT LEARNING>]");
-        System.out.println("[<GIT HUIT LEARNING!>]");
+        System.out.println("I'm happy!");
 
 
         int result = 5 + 10;
